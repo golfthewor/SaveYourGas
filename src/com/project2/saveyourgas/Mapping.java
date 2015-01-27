f8d1b1d302f9d31ef020e8633719234b41603d87
boss <ayouwanno@gmail.com>
2015-01-27 08:14:46 +0700

    Add Map & current position

--- src/com/project2/saveyourgas/Mapping.java
+++ src/com/project2/saveyourgas/Mapping.java
@@ -1,21 +1,40 @@
 package com.project2.saveyourgas;
 
-import android.app.Activity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class Mapping extends Activity{
+public class Mapping extends FragmentActivity{
 	
 	private Intent intent_confirm;
 	private Button btn_confirm;
+	
+	GoogleMap mMap;
+    Marker mMarker;
+    LocationManager lm;
+    double lat, lng;
 	
 		@Override
 		protected void onCreate(Bundle savedInstanceState){
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.mapping);
+			
+			mMap = ((SupportMapFragment)getSupportFragmentManager()
+	                .findFragmentById(R.id.map)).getMap();
 			
 			/*btn_confirm*/
 			btn_confirm = (Button) findViewById(R.id.btn_confirm);
@@ -29,6 +48,67 @@ public class Mapping extends Activity{
 				}
 			});
 		}
+		
+		LocationListener listener = new LocationListener() {
+	        public void onLocationChanged(Location loc) {
+	            LatLng coordinate = new LatLng(loc.getLatitude()
+	                    , loc.getLongitude());
+	            lat = loc.getLatitude();
+	            lng = loc.getLongitude();
+	            
+	            if(mMarker != null)
+	                mMarker.remove();
+	            
+	            mMarker = mMap.addMarker(new MarkerOptions()
+	                    .position(new LatLng(lat, lng)));
+	            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
+	                    coordinate, 16));
+	        }
+
+	        public void onStatusChanged(String provider, int status
+	                , Bundle extras) {}
+	        public void onProviderEnabled(String provider) {}
+	        public void onProviderDisabled(String provider) {}
+	    };
+	    
+	    public void onResume() {
+	        super.onResume();
+	        
+	        lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+	        boolean isNetwork = 
+	                lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+	        boolean isGPS = 
+	                lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+	        if(isNetwork) {
+	            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER
+	                    , 5000, 10, listener);
+	            Location loc = lm.getLastKnownLocation(
+	                    LocationManager.NETWORK_PROVIDER);
+	            if(loc != null) {
+	                lat = loc.getLatitude();
+	                lng = loc.getLongitude();
+	            }
+	        }
+	        
+	        if(isGPS) {
+	            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER
+	                    , 5000, 10, listener);
+	            Location loc = lm.getLastKnownLocation(
+	                    LocationManager.GPS_PROVIDER);
+	            if(loc != null) {
+	                lat = loc.getLatitude();
+	                lng = loc.getLongitude();
+	            }
+	        }
+	    }
+	    
+	    public void onPause() {
+	        super.onPause();
+	        lm.removeUpdates(listener);
+	    }
+
 
 		
 }
