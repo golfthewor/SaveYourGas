e2e16d9d254442cb815c0046c8daf27b8c68c744
boss <ayouwanno@gmail.com>
2015-02-03 04:59:01 +0700

    update mark position in mapping page

--- src/com/project2/saveyourgas/Mapping.java
+++ src/com/project2/saveyourgas/Mapping.java
@@ -2,6 +2,8 @@ package com.project2.saveyourgas;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMarkerClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -17,98 +19,118 @@ import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class Mapping extends FragmentActivity{
-	
+public class Mapping extends FragmentActivity {
+
 	private Intent intent_confirm;
 	private Button btn_confirm;
-	
+
 	GoogleMap mMap;
-    Marker mMarker;
-    LocationManager lm;
-    double lat, lng;
-	
-		@Override
-		protected void onCreate(Bundle savedInstanceState){
-			super.onCreate(savedInstanceState);
-			setContentView(R.layout.mapping);
-			
-			mMap = ((SupportMapFragment)getSupportFragmentManager()
-	                .findFragmentById(R.id.map)).getMap();
-			
-			/*btn_confirm*/
-			btn_confirm = (Button) findViewById(R.id.btn_confirm);
-			intent_confirm = new Intent(getApplicationContext(), Vehical.class);
-			btn_confirm.setOnClickListener(new OnClickListener() {
-				
-				@Override
-				public void onClick(View v) {
-					// TODO Auto-generated method stub
-					startActivity(intent_confirm);
-				}
-			});
+	Marker mMarker;
+	LocationManager lm;
+	double lat, lng;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mapping);
+
+		mMap = ((SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.map)).getMap();
+
+		mMap.setOnMapClickListener(new OnMapClickListener() {
+			public void onMapClick(LatLng arg0) {
+				mMap.addMarker(new MarkerOptions().position(arg0).title(
+						String.valueOf(arg0.latitude) + ", "
+								+ String.valueOf(arg0.longitude)));
+			}
+		});
+
+		mMap.setOnMarkerClickListener(new OnMarkerClickListener() {
+			public boolean onMarkerClick(Marker arg0) {
+				arg0.remove();
+				Toast.makeText(getApplicationContext(),
+						"Remove Marker " + String.valueOf(arg0.getId()),
+						Toast.LENGTH_SHORT).show();
+				return true;
+			}
+		});
+
+		/* btn_confirm */
+		btn_confirm = (Button) findViewById(R.id.btn_confirm);
+		intent_confirm = new Intent(getApplicationContext(), Vehical.class);
+		btn_confirm.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				startActivity(intent_confirm);
+			}
+		});
+	}
+
+	LocationListener listener = new LocationListener() {
+		public void onLocationChanged(Location loc) {
+			LatLng coordinate = new LatLng(loc.getLatitude(),
+					loc.getLongitude());
+			lat = loc.getLatitude();
+			lng = loc.getLongitude();
+
+			if (mMarker != null)
+				mMarker.remove();
+
+			mMarker = mMap.addMarker(new MarkerOptions().position(new LatLng(
+					lat, lng)));
+			mMap.animateCamera(CameraUpdateFactory
+					.newLatLngZoom(coordinate, 16));
 		}
-		
-		LocationListener listener = new LocationListener() {
-	        public void onLocationChanged(Location loc) {
-	            LatLng coordinate = new LatLng(loc.getLatitude()
-	                    , loc.getLongitude());
-	            lat = loc.getLatitude();
-	            lng = loc.getLongitude();
-	            
-	            if(mMarker != null)
-	                mMarker.remove();
-	            
-	            mMarker = mMap.addMarker(new MarkerOptions()
-	                    .position(new LatLng(lat, lng)));
-	            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-	                    coordinate, 16));
-	        }
 
-	        public void onStatusChanged(String provider, int status
-	                , Bundle extras) {}
-	        public void onProviderEnabled(String provider) {}
-	        public void onProviderDisabled(String provider) {}
-	    };
-	    
-	    public void onResume() {
-	        super.onResume();
-	        
-	        lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+		}
 
-	        boolean isNetwork = 
-	                lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-	        boolean isGPS = 
-	                lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		public void onProviderEnabled(String provider) {
+		}
 
-	        if(isNetwork) {
-	            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER
-	                    , 5000, 10, listener);
-	            Location loc = lm.getLastKnownLocation(
-	                    LocationManager.NETWORK_PROVIDER);
-	            if(loc != null) {
-	                lat = loc.getLatitude();
-	                lng = loc.getLongitude();
-	            }
-	        }
-	        
-	        if(isGPS) {
-	            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER
-	                    , 5000, 10, listener);
-	            Location loc = lm.getLastKnownLocation(
-	                    LocationManager.GPS_PROVIDER);
-	            if(loc != null) {
-	                lat = loc.getLatitude();
-	                lng = loc.getLongitude();
-	            }
-	        }
-	    }
-	    
-	    public void onPause() {
-	        super.onPause();
-	        lm.removeUpdates(listener);
-	    }
+		public void onProviderDisabled(String provider) {
+		}
+	};
 
+	public void onResume() {
+		super.onResume();
 
-		
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+		boolean isNetwork = lm
+				.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+		boolean isGPS = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+		if (isNetwork) {
+			lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000,
+					10, listener);
+			Location loc = lm
+					.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+			if (loc != null) {
+				lat = loc.getLatitude();
+				lng = loc.getLongitude();
+			}
+		}
+
+		if (isGPS) {
+			lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10,
+					listener);
+			Location loc = lm
+					.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+			if (loc != null) {
+				lat = loc.getLatitude();
+				lng = loc.getLongitude();
+			}
+		}
+	}
+
+	public void onPause() {
+		super.onPause();
+		lm.removeUpdates(listener);
+	}
+
 }
